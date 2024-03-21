ff42ea1e23c0d35263f5f7cea48aeab3563ab307
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-03-21 16:24:39 +0300

    backtik

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function saturdayFun(activity="roller-skate"){
     
     return `This Saturday, I want to ${activity}!`;
 }
+console.log(saturdayFun("eat"));
 function mondayWork(activity="go to the office"){
     return `This Monday, I will ${activity}.`
 }
